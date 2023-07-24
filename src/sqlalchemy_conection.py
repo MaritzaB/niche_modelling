1614ece9067787c7a922a6a72c7eebc26198fe7a
maritzee <amby.121192@gmail.com>
2023-07-23 21:43:14 -0600

    ✨ query to count distinct states

--- src/sqlalchemy_conection.py
+++ src/sqlalchemy_conection.py
@@ -1,5 +1,5 @@
 import psycopg2
-from sqlalchemy import create_engine, URL, MetaData, Table, select, inspect, desc
+from sqlalchemy import create_engine, URL, MetaData, Table, select, inspect, desc, func
 
 database_url = URL.create(
     "postgresql+psycopg2",
@@ -29,9 +29,16 @@ metadata.reflect(bind=engine)
 # Set connection
 connection = engine.connect()
 
+# Query para ordenar fecha en orden descendente y nombre en forma ascendente
 query = select(*[albatross.columns.date,albatross.columns.name])
 query = query.order_by(desc('date'), 'name')
 print(query)
 results = connection.execute(query).fetchall()
-
 print(results[:10])
+
+# Query para contar distinct names
+print(f'\nQuery para contar distinct names')
+query = select(*[func.count(albatross.columns.name.distinct())])
+print(query)
+results = connection.execute(query).scalar()
+print(results)
