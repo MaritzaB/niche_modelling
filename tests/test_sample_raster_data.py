7521cddc5f1fa7c2313cf725827a4ae39bc3d7d6
maritzée <amby.121192@gmail.com>
2024-03-05 18:20:57 -0600

    ✅ Pass tests

--- tests/test_sample_raster_data.py
+++ tests/test_sample_raster_data.py
@@ -1,12 +1,10 @@
 import pandas as pd
+from pandas.testing import assert_frame_equal
 from ..src.sample_raster_data import get_years_months
 
 file = 'tests/data_tests/points_tests.csv'
 
 def test_get_years_months():
-    expected = pd.DataFrame({'year': [2014, 2018], 'month': [1, 2]})
+    expected = pd.DataFrame({'year': [2014, 2018], 'month': [1, 5]})
     result = get_years_months(file)
-    print(result)
-    print(expected)
-    assert result == expected
-    
+    assert_frame_equal(result, expected)
