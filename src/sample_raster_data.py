266485ca8e747eb0b2067db43f3bf9408712ab72
maritzée <amby.121192@gmail.com>
2024-03-05 20:51:42 -0600

    🚧 update sampling rasterfile

--- src/sample_raster_data.py
+++ src/sample_raster_data.py
@@ -31,54 +31,68 @@ def get_coords_from_multipoints(geom):
     return [(x, y) for x, y in zip(geom.x, geom.y)]
 
 
-def sample_raster(coords, raster_file):
-    src = rasterio.open(raster_file)
-    values_df = pd.DataFrame(columns=['year', 'month', 'lon', 'lat', 'sst'])
+def sample_raster(coords, sst_raster, chla_raster, ew_raster, nw_raster):
+    # Asegurar que las muestras si correspondan a los puntos de la geometría
+    src = rasterio.open(sst_raster)
+    values_df = pd.DataFrame(columns=['year', 'month', 'lon', 'lat', 'sst', 'chla', 'ew', 'nw'])
     values_df['year'] = [year for x in coords]
     values_df['month'] = str(month)
     values_df['lon'] = [x for x, y in coords]
     values_df['lat'] = [y for x, y in coords]
     values_df['sst'] = [val[0] for val in src.sample(coords)]
+    src.close()
+    src = rasterio.open(chla_raster)
+    values_df['chla'] = [val[0] for val in src.sample(coords)]
+    src.close()
+    src = rasterio.open(ew_raster)
+    values_df['ew'] = [val[0] for val in src.sample(coords)]
+    src.close()
+    src = rasterio.open(nw_raster)
+    values_df['nw'] = [val[0] for val in src.sample(coords)]
+    src.close()
     return values_df
 
 
-year = '2014'
-month = '02'
+#year = '2014'
+#month = '02'
+#
 
-file = 'src/data/trajectories.csv'
-sst_tiff = f'src/data/{year}/{month}/processed/{year}_{month}_jplMURSST41__sst.tif'
+#sst_tiff = f'src/data/{year}/{month}/processed/{year}_{month}_jplMURSST41__sst.tif'
 
-def get_sample_values(file, year, month, raster_file):
+def get_sample_values(file, year, month):
     geo_df = csv_to_geodataframe(file)
     geom = subset_multipoints_by_year_month(geo_df, year, month)
     points = explode_multipoints(geom)
     coords = get_coords_from_multipoints(points)
-    values_df = sample_raster(coords, raster_file)
+    sst_raster, chla_raster, ew_raster, nw_raster = select_raster_file(year, month)
+    values_df = sample_raster(coords, sst_raster, chla_raster, ew_raster, nw_raster)
     return values_df
 
 
-def select_raster_file(type, year, month):
+def select_raster_file(year, month):
     dir = f'src/data/{year}/{month}/processed'
-    if type == 'sst':
-        raster = f'{dir}/{year}_{month}_jplMURSST41_reproj.tif'
-    if type == 'chlorophyll':
-        raster = f'{dir}/{year}_{month}_NESDIS_VHNSQ_chla_reproj.tif'
-    if type == 'eastward_wind':
-        raster = f'{dir}/cmems_obs-wind_glo_phy_my_l4_P1M_{year}{month}_clipped_eastward_wind.tif'
-    if type == 'northward_wind':
-        raster = f'{dir}/cmems_obs-wind_glo_phy_my_l4_P1M_{year}{month}_clipped_northward_wind.tif'
-    return raster
+    sst_raster = f'{dir}/{year}_{month}_jplMURSST41_reproj.tif'
+    chla_raster = f'{dir}/{year}_{month}_NESDIS_VHNSQ_chla_reproj.tif'
+    ew_raster = f'{dir}/cmems_obs-wind_glo_phy_my_l4_P1M_{year}{month}_clipped_eastward_wind.tif'
+    nw_raster = f'{dir}/cmems_obs-wind_glo_phy_my_l4_P1M_{year}{month}_clipped_northward_wind.tif'
+    return sst_raster, chla_raster, ew_raster, nw_raster
 
+
+
+#sst_vals = get_sample_values(file, year, month, sst_tiff)
 #raster_types = ['sst', 'chlorophyll', 'eastward_wind', 'northward_wind']
 
+file = 'src/data/trajectories.csv'
 dates = get_years_months(file)
+concatenated_df = pd.DataFrame(columns=['year', 'month', 'lon', 'lat', 'sst', 'chla', 'ew', 'nw'])
 
-#concatenated_df = pd.DataFrame(columns=['year', 'month', 'lon', 'lat', 'sst'])
-#
-#for index, row in dates.iterrows():
-#    year = str(row['year'])
-#    month = str(row['month']).zfill(2)
-#    raster_file = select_raster_file('sst', year, month)
-#    sample_values = get_sample_values(file, year, month, raster_file)
-#    concatenated_df = pd.concat([concatenated_df, sample_values])
+for index, row in dates.iterrows():
+    year = str(row['year'])
+    month = str(row['month']).zfill(2)
+    sample_values = get_sample_values(file, year, month)
+    print('Sample values in ', year, month, ' shape: ', sample_values.shape)
+    concatenated_df = pd.concat([concatenated_df, sample_values])
 
+print('Concatenated shape: ', concatenated_df.shape)
+print(concatenated_df.isna().sum())
+concatenated_df.to_csv('src/data/sst_sample_values.csv', index=False)
